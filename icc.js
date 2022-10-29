390b8602c6b360bd098a9b8433e427d61219959b
AanamDatta <78989806+AanamDatta@users.noreply.github.com>
2022-10-28 22:22:26 -0400

    Add files via upload

---
+++ icc.js
@@ -0,0 +1,26 @@
+status =""
+function preload(){
+    img = loadImage("icc.jpg")
+}
+function setup(){
+    canvas = createCanvas(650,420)
+    canvas.center()
+    ObjectDetector = ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML = "status: detecting objects"
+}
+function modelLoaded(){
+    console.log("model is loaded")
+    status = true
+    ObjectDetector.detect(img, gotresult )
+
+}
+function gotresult(error,results) {
+if (error) {
+    console.log(error)
+} else {
+    console.log(results)
+}
+}
+function draw(){
+    image(img, 0,0,650,420)
+} 
